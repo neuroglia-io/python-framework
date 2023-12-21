7adbe1876aca47da753f54572c91c0cee7cf5058
Charles d'Avernas <charles.davernas@neuroglia.io>
2023-12-21 18:26:00 +0100

    feat(Queryable): Added Queryable support for 'orderby' and 'orderby_descending' clauses

--- src/neuroglia/data/queries/queryable.py
+++ src/neuroglia/data/queries/queryable.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractclassmethod
-from ast import And, Attribute, BoolOp, Call, Compare, Constant, Eq, Gt, GtE, In, Is, IsNot, Lambda, Lt, LtE, Name, Not, NotEq, NotIn, Or, Subscript, USub, UnaryOp, boolop, cmpop, expr
+from ast import And, Attribute, BoolOp, Call, Compare, Constant, Eq, Gt, GtE, In, Is, IsNot, Lambda, Lt, LtE, Name, NodeVisitor, Not, NotEq, NotIn, Or, Subscript, USub, UnaryOp, arg, boolop, cmpop, expr
 import ast
 from dataclasses import dataclass
 import importlib
 import inspect
 from pymongo import MongoClient
+import pymongo
 from pymongo.collection import Collection
-from typing import Callable, Dict, Generic, List, Optional, Type, TypeVar
+from typing import Any, Callable, Dict, Generic, List, Optional, Type, TypeVar
 from pymongo.cursor import Cursor
 from pymongo.database import Database
 
@@ -36,7 +37,7 @@ class Queryable(Generic[T]):
 
     def __init__(self, provider : QueryProvider, expression : Optional[expr] = None):
         self.provider = provider
-        self.expression = ast.Constant(value = self) if expression is None else expression
+        self.expression = ast.Name(value = 'query') if expression is None else expression
 
     expression: expr
     ''' Gets the expression that is associated with the queryable '''
@@ -45,6 +46,58 @@ class Queryable(Generic[T]):
     ''' Gets the service used to create and execute queries associated with the data source '''
     
     def get_element_type(self) -> Type: return self.__orig_class__.__args__[0]
+
+    def orderby(self, attribute_selector: Callable[[T], Any]):
+        element_type = self.get_element_type()
+        element_type_qualified_name = f'{element_type.__module__}.{element_type.__name__}'
+        
+        attribute_selector_source = inspect.getsource(attribute_selector)
+        attribute_selector_tree = ast.parse(attribute_selector_source)
+        attribute_selector_expression = attribute_selector_tree.body[0].value     
+        attribute_selector_lambda_expression = attribute_selector_expression.args[0]
+        if not isinstance(attribute_selector_lambda_expression.body, Attribute): raise Exception("The specified expression must be of type Attribute")
+        
+        attribute_selector_lambda_argument_expression = attribute_selector_lambda_expression.args.args[0]
+        attribute_selector_lambda_argument_expression.annotation = ast.Name(element_type_qualified_name, ctx=ast.Load())
+        
+        func = ast.Attribute(
+            value=self.expression,
+            attr='orderby',
+            ctx=ast.Load())
+        args = [ attribute_selector_lambda_expression ]
+        keywords = []
+        expression = ast.Call(
+            func = func, 
+            args = args,
+            keywords = keywords)
+
+        return self.provider.create_query(self.get_element_type(), expression)
+    
+    def orderby_descending(self, attribute_selector: Callable[[T], Any]):
+        element_type = self.get_element_type()
+        element_type_qualified_name = f'{element_type.__module__}.{element_type.__name__}'
+        
+        attribute_selector_source = inspect.getsource(attribute_selector)
+        attribute_selector_tree = ast.parse(attribute_selector_source)
+        attribute_selector_expression = attribute_selector_tree.body[0].value     
+        attribute_selector_lambda_expression = attribute_selector_expression.args[0]
+        if not isinstance(attribute_selector_lambda_expression.body, Attribute): raise Exception("The specified expression must be of type Attribute")
+        
+        attribute_selector_lambda_argument_expression = attribute_selector_lambda_expression.args.args[0]
+        attribute_selector_lambda_argument_expression.annotation = ast.Name(element_type_qualified_name, ctx=ast.Load())
+        
+        func = ast.Attribute(
+            value=self.expression,
+            attr='orderby_descending',
+            ctx=ast.Load())
+        args = [ attribute_selector_lambda_expression ]
+        keywords = []
+        expression = ast.Call(
+            func = func, 
+            args = args,
+            keywords = keywords)
+
+        return self.provider.create_query(self.get_element_type(), expression)
 
     def where(self, predicate: Callable[[T], bool]) -> 'Queryable[T]':
         
@@ -62,7 +115,7 @@ class Queryable(Generic[T]):
             value=self.expression,
             attr='where',
             ctx=ast.Load())
-        args = [ self, predicate_expression ]
+        args = [ predicate_lambda_expression ]
         keywords = []
         expression = ast.Call(
             func = func, 
@@ -92,69 +145,29 @@ class MongoQueryProvider(QueryProvider):
     def create_query(self, element_type: Type, expression : expr) -> 'Queryable': return MongoQuery[element_type](self, expression)
     
     def execute(self, expression : expr) -> any:
-        translator = MongoQueryTranslator()
-        context = MongoQueryEvaluationContext(self._collection.find())
-        translator.translate(expression, context)
-        return list(context.query)
+        query = MongoQueryBuilder(self._collection, JavascriptExpressionTranslator()).build(expression)
+        return list(query)
 
 
 
-class MongoQueryEvaluationContext:
-        
-    def __init__(self, query : Cursor):
-        self.query = query
+class JavascriptExpressionTranslator:
 
-    query : Cursor
-
-
-
-@dataclass
-class MongoQueryEvaluationScope:
-    
-    context: MongoQueryEvaluationContext
-  
-    arguments : Dict[str, Type]
-    
-    parent_scope: Optional['MongoQueryEvaluationScope'] = None
-   
-    def get_all_arguments(self) -> Dict[str, Type]:
-        arguments = dict[str, Type]() if self.parent_scope is None else self.parent_scope.get_all_arguments()
-        if self.arguments is not None: arguments.update(self.arguments)
-        return arguments
-
-
-
-class MongoQueryTranslator:
-    
-    def translate(self, expression: expr, context: MongoQueryEvaluationContext) -> None:
-        if isinstance(expression, Constant): return context.query_options
-        elif isinstance(expression, Call):
-            declaring_type = getattr(expression.func.value, 'id', None)
-            if declaring_type is None: declaring_type = expression.args[0].__class__.__name__# extract from args if id does not exist -> chained calls
-            if declaring_type != 'Queryable' and declaring_type != 'MongoQuery': raise Exception(f"The translator expects query expressions of type 'Call' to invoke only functions declared by the 'Queryable' or 'MongoQuery' types, but a function declared by '{declaring_type}' was supplied")
-            function_name = expression.func.attr
-            lambda_expression = expression.args[1].args[0]
-            if function_name == 'where': self.translate_where_clause(lambda_expression, context)
-        else: raise Exception(f"The translator expects query expressions of type 'Call' or type 'Constant', but an expression of type '{type(expression).__name__}' was supplied")
-    
-    def translate_where_clause(self, expression: expr, context: MongoQueryEvaluationContext) -> None:
-        if not isinstance(expression, Lambda): raise Exception(f"The 'where' clause translator expects an expression of type 'Lambda', but an expression of type '{type(expression).__name__}' was supplied")
-        scope = MongoQueryEvaluationScope(context, self._get_lambda_arguments(expression))
-        expression = self.translate_expression(expression.body, scope)
-        context.query.where(expression)
-        
-    def translate_expression(self, expression: expr, scope : MongoQueryEvaluationScope) -> str:
-        if isinstance(expression, Attribute): return self.translate_attribute(expression, scope)
-        elif isinstance(expression, BoolOp): return self.translate_bool_op(expression, scope)
-        elif isinstance(expression, Call):  return self.translate_call(expression, scope)
-        elif isinstance(expression, Compare): return self.translate_compare(expression, scope)
-        elif isinstance(expression, Constant): return self.translate_constant(expression, scope)
-        elif isinstance(expression, Name): return self.translate_name(expression, scope)
-        elif isinstance(expression, Subscript): return self.translate_subscript(expression, scope)
-        elif isinstance(expression, UnaryOp): return self.translate_unary_op(expression, scope)
+    def translate(self, expression: expr) -> str:
+        if isinstance(expression, arg): return self.translate_arg(expression)
+        elif isinstance(expression, Attribute): return self.translate_attribute(expression)
+        elif isinstance(expression, BoolOp): return self.translate_bool_op(expression)
+        elif isinstance(expression, Call):  return self.translate_call(expression)
+        elif isinstance(expression, Compare): return self.translate_compare(expression)
+        elif isinstance(expression, Constant): return self.translate_constant(expression)
+        elif isinstance(expression, Lambda): return self.translate_lambda(expression)
+        elif isinstance(expression, Name): return self.translate_name(expression)
+        elif isinstance(expression, Subscript): return self.translate_subscript(expression)
+        elif isinstance(expression, UnaryOp): return self.translate_unary_op(expression)
         else: raise Exception(f"The specified expression type '{type(expression)}' is not supported in this context")
-        
-    def translate_attribute(self, expression: Attribute, scope: MongoQueryEvaluationScope) -> str:
+
+    def translate_arg(self, expression: arg) -> str: return expression.arg
+
+    def translate_attribute(self, expression: Attribute) -> str:
         attribute_name : str = None
         attribute_path_parts = list[str]()
         try: 
@@ -168,27 +181,27 @@ class MongoQueryTranslator:
                 except:
                     attribute_path_parts.append(expression.value.attr)
                     expression = expression.value
-        if scope.get_all_arguments().get(attribute_name) is None: return self._evaluate(expression)
+        #if scope.get_all_arguments().get(attribute_name) is None: return self._evaluate(expression) #todo
         attribute_path_parts.reverse()
         attribute_full_name = f"this.{'.'.join(attribute_path_parts)}"
         return attribute_full_name
 
-    def translate_bool_op(self, expression: BoolOp, scope: MongoQueryEvaluationScope) -> str:
+    def translate_bool_op(self, expression: BoolOp) -> str:
         operator = self._translate_boolop(expression.op)
-        parameters = [ self.translate_expression(parameter, scope) for parameter in expression.values ]
+        parameters = [ self.translate(parameter) for parameter in expression.values ]
         return f' {operator} '.join(parameters)        
 
-    def translate_call(self, expression: Call, scope: MongoQueryEvaluationScope) -> str:
+    def translate_call(self, expression: Call) -> str:
         function_name = getattr(expression.func, 'id', None)
         function_body : str
         function_args : List[str]
         if function_name is None:
             function_name = expression.func.attr
-            function_body = self.translate_expression(expression.func.value, scope)
-            function_args = [self.translate_expression(arg, scope) for arg in expression.args]
+            function_body = self.translate(expression.func.value)
+            function_args = [self.translate(arg) for arg in expression.args]
         else:
-            function_body = self.translate_expression(expression.args[0], scope)
-            function_args = [self.translate_expression(arg) for arg in expression.args[1:]]
+            function_body = self.translate(expression.args[0])
+            function_args = [self.translate(arg) for arg in expression.args[1:]]
         if function_name == 'count': return f"{function_body}.length"
         elif function_name == 'len': return f"{function_body}.length"
         elif function_name == 'lower': return f"{function_body}.toLowerCase()"
@@ -199,33 +212,39 @@ class MongoQueryTranslator:
         elif function_name == 'split': return f"{function_body}.split({', '.join(function_args)})"
         else: raise Exception(f"The specified function '{function_name}' is not supported in this context")
     
-    def translate_compare(self, expression: Compare, scope: MongoQueryEvaluationScope) -> str:
-        left = self.translate_expression(expression.left, scope)        
-        right = self.translate_expression(expression.comparators[0], scope)
+    def translate_compare(self, expression: Compare) -> str:
+        left = self.translate(expression.left)        
+        right = self.translate(expression.comparators[0])
         operator = self._translate_cmpop(expression.ops[0])
         if operator == 'in': return f"{right}.includes({left})"
         elif operator == 'not in': return f"!{right}.includes({left})"
         else: return f"{left} {operator} {right}"
 
-    def translate_constant(self, expression: Constant, scope: MongoQueryEvaluationScope) -> str:
+    def translate_constant(self, expression: Constant) -> str:
         if expression.value is None: return 'null'
         if isinstance(expression.value, str): return f"'{expression.value}'"
         else: return str(expression.value)
         
-    def translate_name(self, expression: Name, scope: MongoQueryEvaluationScope) -> str:
+    def translate_lambda(self, expression: Lambda) -> str:
+        body = self.translate(expression.body)
+        args = [arg.arg for arg in expression.args.args]
+        for arg in args: body = body.replace(arg, 'this')
+        return body
+
+    def translate_name(self, expression: Name) -> str:
         return expression.id
     
-    def translate_subscript(self, expression: Subscript, scope: MongoQueryEvaluationScope) -> str:
-        source = self.translate_expression(expression.value, scope)
-        slice = self.translate_expression(expression.slice, scope)
+    def translate_subscript(self, expression: Subscript) -> str:
+        source = self.translate(expression.value)
+        slice = self.translate(expression.slice)
         slice_index : int = None
         try: slice_index = int(slice)
         except: pass
         if slice_index is not None and slice_index < 0: return f"{source}.slice({slice})"
         else: return f"{source}[{slice}]"
     
-    def translate_unary_op(self, expression: UnaryOp, scope: MongoQueryEvaluationScope) -> str:
-        operand = self.translate_expression(expression.operand, scope)
+    def translate_unary_op(self, expression: UnaryOp) -> str:
+        operand = self.translate(expression.operand)
         if isinstance(expression.op, Not): return f"!({operand})"
         elif isinstance(expression.op, USub): return f"-{operand}"
         else: raise Exception(f"The specified unary operator '{type(expression.op).__name__}' is not supported in this context")
@@ -266,6 +285,43 @@ class MongoQueryTranslator:
 
 
 
+
+class MongoQueryBuilder(NodeVisitor):
+   
+    def __init__(self, collection : Collection, translator : JavascriptExpressionTranslator):
+        self._collection = collection
+        self._translator = translator
+
+    _collection : Collection
+
+    _translator : JavascriptExpressionTranslator
+    
+    _select_clause : Optional[Dict];
+    
+    _where_clauses : List[str] = list[str]()
+    
+    _orderby_clauses : Dict[str, int] = dict[str, int]()
+
+    def build(self, expression : expr) -> Cursor:
+        self.visit(expression)
+        cursor : Cursor = self._collection.find() #projection = None if self._select_clause is None else self._select_clause)
+        where = ' && '.join(self._where_clauses)
+        cursor = cursor.where(where)
+        if len(self._orderby_clauses) > 0: cursor = cursor.sort(self._orderby_clauses) 
+        return cursor
+    
+    def visit_Call(self, node):
+        clause = node.func.attr
+        expression = node.args[0]
+        self.visit(node.func.value)
+        javascript = self._translator.translate(expression)
+        if clause == 'orderby': self._orderby_clauses[javascript.replace('this.', '')] = pymongo.ASCENDING
+        elif clause == 'orderby_descending': self._orderby_clauses[javascript.replace('this.', '')] = pymongo.DESCENDING
+        elif clause == 'select': self._select_clause = javascript
+        elif clause == 'where': self._where_clauses.append(javascript)
+        
+
+
 @dataclass
 class User:
     
@@ -273,24 +329,28 @@ class User:
     
     email: str = None
 
+
 mongo_client = MongoClient('mongodb://localhost:27017')
 collection = mongo_client.get_database('test').get_collection('users')
 query_provider = MongoQueryProvider(collection)
 query = MongoQuery[User](query_provider)
-query = query.where(lambda user: user.name is None)
-query = query.where(lambda user: user.name == 'John Doe' 
-                    and user.email != None 
-                    and len(user.email) > 3 
-                    and user.name.lower() == 'john doe' 
-                    and user.name.upper() == 'JOHN DOE' 
-                    and user.name[0] == "J" 
-                    and user.name.startswith('John') 
-                    and user.name.endswith('Doe') 
-                    and 'Doe' in user.name
-                    and 'Jane' not in user.name
-                    and user.name.count() > 0
-                    and user.email.split('@')[-1] != 'mailinator'
-                    and user.name.index('J') == 0)
+#query = query.where(lambda user: user.name is None)
+query = query.where(lambda user: user.name != 'OK')
+# query = query.where(lambda user: user.name == 'John Doe' 
+#                     and user.email != None 
+#                     and len(user.email) > 3 
+#                     and user.name.lower() == 'john doe' 
+#                     and user.name.upper() == 'JOHN DOE' 
+#                     and user.name[0] == "J" 
+#                     and user.name.startswith('John') 
+#                     and user.name.endswith('Doe') 
+#                     and 'Doe' in user.name
+#                     and 'Jane' not in user.name
+#                     and user.name.count() > 0
+#                     and user.email.split('@')[-1] != 'mailinator'
+#                     and user.name.index('J') == 0)
+query = query.orderby_descending(lambda user: user.name)
+query = query.orderby(lambda user: user.email)
 results = query.to_list()
 
 query = query.where(lambda user: user.name == 'Jane Doe' and user.email != None and len(user.email) > 3)
