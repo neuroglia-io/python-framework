f58b70dec605e7fe40664332ddb58e782fa2c1a6
Bruno van de Werve <bvandewe@cisco.com>
2024-01-23 16:35:34 +0100

    WIP

--- samples/openbank/api/main.py
+++ samples/openbank/api/main.py
@@ -3,7 +3,7 @@ from neuroglia.data.infrastructure.event_sourcing.abstractions import EventStore
 from neuroglia.data.infrastructure.event_sourcing.event_sourcing_repository import EventSourcingRepository
 from neuroglia.data.infrastructure.event_sourcing.event_store.event_store import ESEventStore
 from neuroglia.data.infrastructure.mongo.mongo_repository import MongoRepository
-from neuroglia.eventing.cloud_events.infrastructure import  CloudEventIngestor, CloudEventMiddleware
+from neuroglia.eventing.cloud_events.infrastructure import CloudEventIngestor, CloudEventMiddleware
 from neuroglia.hosting.configuration.data_access_layer import DataAccessLayer
 from neuroglia.hosting.web import WebApplicationBuilder
 from neuroglia.mapping.mapper import Mapper
@@ -18,16 +18,16 @@ builder = WebApplicationBuilder()
 
 Mapper.configure(builder, [application_module])
 Mediator.configure(builder, [application_module])
-CloudEventIngestor.configure(builder, [application_module]) #replace application_module with name of module(s) defining cloud events to handle
+CloudEventIngestor.configure(builder, [application_module])  # replace application_module with name of module(s) defining cloud events to handle
 ESEventStore.configure(builder, EventStoreOptions(database_name))
-DataAccessLayer.WriteModel.configure(builder, [ "samples.openbank.domain.models" ], lambda builder_, entity_type, key_type: EventSourcingRepository.configure(builder_, entity_type, key_type))
-DataAccessLayer.ReadModel.configure(builder, [ "samples.openbank.integration.models" ], lambda builder_, entity_type, key_type: MongoRepository.configure(builder_, entity_type, key_type, database_name))
+DataAccessLayer.WriteModel.configure(builder, ["samples.openbank.domain.models"], lambda builder_, entity_type, key_type: EventSourcingRepository.configure(builder_, entity_type, key_type))
+DataAccessLayer.ReadModel.configure(builder, ["samples.openbank.integration.models"], lambda builder_, entity_type, key_type: MongoRepository.configure(builder_, entity_type, key_type, database_name))
 JsonSerializer.configure(builder)
 builder.add_controllers(["samples.openbank.api.controllers"])
 
 app = builder.build()
 
-#app.add_middleware(ExceptionHandlingMiddleware, service_provider=app.services) #todo: uncomment
+# app.add_middleware(ExceptionHandlingMiddleware, service_provider=app.services) #todo: uncomment
 app.add_middleware(CloudEventMiddleware, service_provider=app.services)
 app.use_controllers()
 
